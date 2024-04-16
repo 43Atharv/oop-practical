8d76cdc7e71db2f491d30fde19e1a71005f4492d
Atharv Chougule <154349297+43Atharv@users.noreply.github.com>
2024-04-16 10:59:57 +0530

    Add files via upload

---
+++ movie.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class moviename
+{
+	string Moviename;
+	int ticketprice;	
+	public: Movie(){
+	
+	    static int moviecount=0;
+	    
+	}
+	void moviebook()
+ {
+    moviecount++;
+ };
+ int main()
+   string Moviename;
+   cout<<"Enter the movie name:"<<endl;
+   cin>>Moviename;
+   cout<<"Price of tickets:"<<endl;
+   cin>>ticketprice;
+   
+   
+   
+   
+}
+	
